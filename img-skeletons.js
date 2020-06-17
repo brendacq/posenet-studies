f0881ee55bffd63ff20cfc128445f09232315440
brenda <bcq.cpp@gmail.com>
2020-06-16 23:05:30 -0300

    loading model and posenet configs

--- img-skeletons.js
+++ img-skeletons.js
@@ -4,11 +4,26 @@ let posenet;
 function setup() {
     createCanvas(640, 480);
 
-    img = createImg('img/image3.jpg');
+    img = createImg('image3.jpg', imageReady);
     img.hide();
     //img.size(640, 480);
+    
+}
+
+function imageReady(){
+    posenet = ml5.poseNet(modelReady);
+
+    posenet.on('pose', (poses) => {
+      console.log(poses);
+    });
+}
+
+function modelReady(){
+    console.log('model loaded');
+
+    posenet.singlePose(img);
 }
 
 function draw() {
-    image(img, 0, 0, width, height);
+    image(img, 0, 0, width, height);  
 }
