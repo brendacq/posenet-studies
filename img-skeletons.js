6bdc5257cf4915a761633822878e6d8543dc19f0
brenda <bcq.cpp@gmail.com>
2020-06-16 22:35:53 -0300

    not working

--- img-skeletons.js
+++ img-skeletons.js
@@ -1,16 +1,27 @@
 let img;
 let poseNet;
+let pose;
 
 function setup() {
     createCanvas(640, 480);
 
-    img = createImg('img/image3.jpg', 'athlete', '');
+    img = createImg('./img/image3.jpg', ' ', '');
+    img.size(width, height);
     img.hide();
+
     poseNet = ml5.poseNet(img, () => {
         console.log('model loaded');
+        poseNet.singlePose(img);
+    });
+
+    poseNet.on('pose', poses => {
+        console.log(poses);
+        if(poses.length > 0){
+            pose = poses[0].pose;
+        }
     });
 }
 
 function draw() {
-    image(img, 0, 0);
+    image(img, 0, 0, width, height);
 }
