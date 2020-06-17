40251c52d6fd44fe426009801cd2ba77d5c0429e
brenda <bcq.cpp@gmail.com>
2020-06-16 22:36:43 -0300

    starting over

--- img-skeletons.js
+++ img-skeletons.js
@@ -1,27 +1,7 @@
-let img;
-let poseNet;
-let pose;
-
 function setup() {
     createCanvas(640, 480);
-
-    img = createImg('./img/image3.jpg', ' ', '');
-    img.size(width, height);
-    img.hide();
-
-    poseNet = ml5.poseNet(img, () => {
-        console.log('model loaded');
-        poseNet.singlePose(img);
-    });
-
-    poseNet.on('pose', poses => {
-        console.log(poses);
-        if(poses.length > 0){
-            pose = poses[0].pose;
-        }
-    });
 }
 
 function draw() {
-    image(img, 0, 0, width, height);
+    
 }
