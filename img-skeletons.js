76260d3b073efcab6a779657e90d4c0d43a56eb3
brenda <bcq.cpp@gmail.com>
2020-06-16 22:42:14 -0300

    image loaded

--- img-skeletons.js
+++ img-skeletons.js
@@ -1,7 +1,14 @@
+let img;
+let posenet;
+
 function setup() {
     createCanvas(640, 480);
+
+    img = createImg('img/image3.jpg');
+    img.hide();
+    //img.size(640, 480);
 }
 
 function draw() {
-    
+    image(img, 0, 0, width, height);
 }
