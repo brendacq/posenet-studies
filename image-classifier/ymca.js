b8439228601a05e167157ec9ddd699693634b8b5
brenda <bcq.cpp@gmail.com>
2020-06-29 21:40:38 -0300

    correcting x and y inputs

--- image-classifier/ymca.js
+++ image-classifier/ymca.js
@@ -6,34 +6,34 @@ let skeleton;
 
 let brain;          // neural network;
 
-let state = 'waiting...';
-let targetLabel;
+// let state = 'waiting...';
+// let targetLabel;
 
 // To give us time to take place before the data start being collected, we use the JS function setTimeout() that will execute a
 // function after the time we set - in this case, 1 second. 
-function keyPressed() {
+// function keyPressed() {
 
-    if (key == 's') {
-        // To save the data as a json file
-        brain.saveData();
-    } else {
-        targetLabel = key;
-        console.log(targetLabel);
+//     if (key == 's') {
+//         // To save the data as a json file
+//         brain.saveData();
+//     } else {
+//         targetLabel = key;
+//         console.log(targetLabel);
 
-        setTimeout(timing, 1000);
-    }
-}
+//         setTimeout(timing, 1000);
+//     }
+// }
 
 // Let's set some time to stop the data collecting - 10 seconds after it starts collecting.
-function timing() {
-    console.log('collecting');
-    state = 'collecting';
+// function timing() {
+//     console.log('collecting');
+//     state = 'collecting';
 
-    setTimeout(() => {
-        state = 'waiting...';
-        console.log('not collecting');
-    }, 10000);
-}
+//     setTimeout(() => {
+//         state = 'waiting...';
+//         console.log('not collecting');
+//     }, 10000);
+// }
 
 function setup() {
     createCanvas(640, 480);
@@ -62,9 +62,9 @@ function setup() {
 
     //Create an object to store the 3 files from the model
     const modelInfo = {
-        model: 'model/model.json',
-        metadata: 'model/model_meta.json',
-        weights:'model/model.weights.bin'
+        model: './model/model.json',
+        metadata: './model/model_meta.json',
+        weights:'./model/model.weights.bin'
     }
 
     // Load the model trained:
@@ -83,15 +83,15 @@ function classifyPose() {
         let inputs = [];
 
         for (var i = 0; i < pose.keypoints.length; i++) {
-            let x = pose.keypoints[i].x;
-            let y = pose.keypoints[i].y;
+            let x = pose.keypoints[i].position.x;
+            let y = pose.keypoints[i].position.y;
             inputs.push(x);
             inputs.push(y);
         }
-
+        console.log('inputs done')
         brain.classify(inputs, gotResults);
     } else {
-        setTimeout(classifyPose, 400);
+        setTimeout(classidex.htmlfyPose, 400);
     }
 }
 
@@ -104,17 +104,17 @@ function gotResults(error, results) {
 
 // Beforing training the data, we need to format it so it'll fit the range from 0 to 1. The normalizeData() method can be used to facilitate the process.
 // After normalizing the data, we can train the model with the train() method.
-function dataLoaded() {
-    console.log('data loaded');
-    brain.normalizeData();
-    brain.train({ epochs: 50 }, finishedTraining);
-}
+// function dataLoaded() {
+//     console.log('data loaded');
+//     brain.normalizeData();
+//     brain.train({ epochs: 50 }, finishedTraining);
+// }
 
-function finishedTraining() {
-    console.log('model trained');
-    // After training the model, saves the files.
-    brain.save();
-}
+// function finishedTraining() {
+//     console.log('model trained');
+//     // After training the model, saves the files.
+//     brain.save();
+// }
 
 function gotPose(poses) {
     // console.log(poses);
@@ -124,23 +124,23 @@ function gotPose(poses) {
         skeleton = poses[0].skeleton;
 
         // The inputs should be collected and fed to the neural net only if a key is pressed. This is how we assure that:
-        if (state == "collecting") {
-            // To flatten the data, we need to put it on a plain array. Using the push() method, we're gonna save the x and y coordinates
-            // to each keypoint in the inputs array, so it'll have 17 positions with 2 informations each: the x and y location.
-            let inputs = [];
-            let target = [targetLabel];
+        // if (state == "collecting") {
+        //     // To flatten the data, we need to put it on a plain array. Using the push() method, we're gonna save the x and y coordinates
+        //     // to each keypoint in the inputs array, so it'll have 17 positions with 2 informations each: the x and y location.
+        //     let inputs = [];
+        //     let target = [targetLabel];
 
-            for (var i = 0; i < pose.keypoints.length; i++) {
-                let x = pose.keypoints[i].position.x;
-                let y = pose.keypoints[i].position.y;
+        //     for (var i = 0; i < pose.keypoints.length; i++) {
+        //         let x = pose.keypoints[i].position.x;
+        //         let y = pose.keypoints[i].position.y;
 
-                inputs.push(x);
-                inputs.push(y);
-            }
+        //         inputs.push(x);
+        //         inputs.push(y);
+        //     }
 
-            // After handling the data, we need to add it to our neural network.
-            brain.addData(inputs, target);
-        }
+        //     // After handling the data, we need to add it to our neural network.
+        //     brain.addData(inputs, target);
+        // }
     }
 }
 
