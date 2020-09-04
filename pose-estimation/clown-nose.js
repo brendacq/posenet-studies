89b0ed70a8dca12f15ae16c883501eeead221ffa
brenda <brendacq@ufrn.edu.br>
2020-09-04 01:34:13 -0300

    Face Keypoints and clown nose

---
+++ pose-estimation/clown-nose.js
@@ -0,0 +1,41 @@
+let video;
+let poseNet;
+
+let noseX = 0;
+let noseY = 0;
+
+let leftEyeX = 0;
+let leftEyeY = 0;
+
+function setup() {
+    createCanvas(640, 480);
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, () => {
+        console.log('model ready')
+    });
+
+    poseNet.on('pose', (poses) => {
+        // console.log(poses);
+        if (poses) {
+            let newX = poses[0].pose.keypoints[0].position.x;
+            let newY = poses[0].pose.keypoints[0].position.y;
+            leftEyeX = poses[0].pose.keypoints[1].position.x;
+            leftEyeY = poses[0].pose.keypoints[1].position.y;
+
+            noseX = lerp(noseX, newX, 0.7);
+            noseY = lerp(noseY, newY, 0.7);
+        }
+    });
+}
+
+
+function draw() {
+    image(video, 0, 0);
+
+    let d = dist(noseX, noseY, leftEyeX, leftEyeY);
+
+    fill(255, 0, 0);
+    ellipse(noseX, noseY, d);
+}
